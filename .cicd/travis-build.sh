2c17a4c7d7e43bead902b7a723dcea19d539d5bf
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-29 13:14:09 -0400

    Correct typo on docker run command.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -19,5 +19,5 @@ if [[ "$(uname)" == Darwin ]]; then
     echo "$ ctest -j$MAKE_PROC_LIMIT -LE _tests --output-on-failure -T Test"
     ctest -j$MAKE_PROC_LIMIT -LE _tests --output-on-failure -T Test # run unit tests
 else # linux
-    execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e MAKE_PROC_LIMIT-e CCACHE_DIR=/opt/.ccache $FULL_TAG
+    execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e MAKE_PROC_LIMIT -e CCACHE_DIR=/opt/.ccache $FULL_TAG
 fi
